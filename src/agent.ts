5ffaa76944300d762e16505be44af541a61eb958
Ivan Saez Zamora <ivsaez@gmail.com>
2022-04-06 17:33:58 +0200

    implement agent

---
+++ src/agent.ts
@@ -0,0 +1,218 @@
+import { Aspect, Likes } from "npc-aspect";
+import { RelationSet } from "npc-relations";
+import { Individual, TruthTable } from "first-order-logic";
+import { IEmotional } from "npc-emotional";
+import { Happiness, Personality } from "npc-mind";
+import internal from "stream";
+
+export type PassTurn = (turn: number, agent: Agent) => string;
+
+export class Agent implements IEmotional{
+    private _name: string;
+    private _aspect: Aspect;
+    private _likes: Likes;
+    private _relations: RelationSet;
+    private _happiness: Happiness;
+    private _personality: Personality;
+    private _isHuman: boolean;
+    private _characteristics: TruthTable;
+    private _desires: Desires;
+    private _onTurnPassed: PassTurn;
+
+    constructor(
+        name: string,
+        aspect: Aspect,
+        relations: RelationSet,
+        happiness: Happiness,
+        personality: Personality,
+        likes: Likes,
+        characteristics: TruthTable,
+        human: boolean,
+        onTurnPassed: PassTurn = null
+    ){
+        if(name == null || name.trim() === "")
+            throw new Error("Name cannot be empty.");
+        
+        if(aspect == null)
+            throw new Error("Aspect cannot be null.");
+        
+        if(relations == null)
+            throw new Error("Relations cannot be null.");
+        
+        if(happiness == null)
+            throw new Error("Happiness cannot be null.");
+        
+        if(personality == null)
+            throw new Error("Personality cannot be null.");
+        
+        if(likes == null)
+            throw new Error("Likes cannot be null.");
+        
+        if(characteristics == null)
+            throw new Error("Characteristics cannot be null.");
+        
+        this._name = name;
+        this._aspect = aspect;
+        this._relations = relations;
+        this._happiness = happiness;
+        this._personality = personality;
+        this._likes = likes;
+        this._characteristics = characteristics;
+        this._desires = new Desires();
+        this._isHuman = human;
+        this._onTurnPassed = onTurnPassed == null 
+            ? (turn: number, agent: Agent) => "" 
+            : onTurnPassed;
+    }
+
+    get Name(){
+        return this._name;
+    }
+
+    get Aspect(){
+        return this._aspect;
+    }
+
+    get Likes(){
+        return this._likes;
+    }
+
+    get Relations(){
+        return this._relations;
+    }
+
+    get Happiness(){
+        return this._happiness;
+    }
+
+    get Personality(){
+        return this._personality;
+    }
+
+    get IsHuman(){
+        return this._isHuman;
+    }
+
+    get Characteristics(){
+        return this._characteristics;
+    }
+
+    get Desires(){
+        return this._desires;
+    }
+
+    get OnTurnPassed(){
+        return this._onTurnPassed;
+    }
+
+    get Individual(): Individual{
+        return new Individual(this.Name.replace(" ", ""));
+    }
+
+    say(content: string){
+        if(content == null)
+            throw new Error("Content cannot be null.");
+        
+        return `${this.Name}: - ${content}`;
+    }
+
+    copy(): Agent{
+        return new Agent(
+            this._name,
+            this._aspect.copy(),
+            this._relations.copy(),
+            this._happiness.copy(),
+            this._personality.copy(),
+            this._likes.copy(),
+            this._characteristics.copy(),
+            this._isHuman
+        );
+    }
+
+    passTurn(turn: number): string{
+        return this._onTurnPassed(turn, this);
+    }
+}
+
+export class Crowd{
+    private _crowd: Map<string, Agent>;
+
+    constructor(agents: Agent[]){
+        if(agents == null)
+            throw new Error("Agents cannot be null.");
+        
+        this._crowd = new Map<string, Agent>();
+        for(let agent of agents){
+            this._crowd.set(agent.Name, agent);
+        }
+    }
+
+    get all(): Agent[]{
+        return [...this._crowd.values()];
+    }
+
+    get(name: string): Agent{
+        return this._crowd.get(name);
+    }
+
+    copy(): Crowd{
+        return new Crowd(this.all.map(agent => agent.copy()));
+    }
+}
+
+export type Heuristic = (crowd: Crowd) => number;
+
+export class Desire{
+    private _involved: Set<string>;
+    private _heuristic: Heuristic;
+
+    constructor(heuristic: Heuristic, involved: string[]){
+        if(heuristic == null)
+            throw new Error("Heuristic cannot be null.");
+        
+        if(involved == null)
+            throw new Error("Involved agents cannot be null.");
+        
+        this._heuristic = heuristic;
+        this._involved = new Set<string>(involved);
+    }
+
+    get heuristic(){
+        return this._heuristic;
+    }
+
+    anyInvolved(agents: Agent[]){
+        return agents.some(agent => this._involved.has(agent.Name));
+    }
+}
+
+export class Desires{
+    private _desires: Desire[];
+
+    constructor(){
+        this._desires = [];
+    }
+
+    get any(): boolean{
+        return this._desires.length > 0;
+    }
+
+    append(desire: Desire): Desires{
+        if(desire != null)
+            this._desires.push(desire);
+        
+        return this;
+    }
+
+    clear(): void{
+        this._desires = [];
+    }
+
+    anyInvolved(agents: Agent[]): boolean{
+        return this._desires.some(desire => desire.anyInvolved(agents));
+    }
+
+    heuristicTotal(crowd: Crowd){
+        return this._desires.reduce((accumulated, desire) => accumulated + desire.heuristic(crowd), 0);
+    }
+}
---
+++ src/tests/agent.test.ts
@@ -0,0 +1,111 @@
+import { Agent, Desire, Crowd, Desires } from "../agent";
+import { Aspect, Likes, OriginKind } from "npc-aspect";
+import { RelationSet } from "npc-relations";
+import { Happiness, Personality } from "npc-mind";
+import { Individual, TruthTable } from "first-order-logic";
+
+function generateAgent(name: string): Agent{
+    return new Agent(
+        name,
+        Aspect.generateOriginHuman(OriginKind.Oceanic),
+        new RelationSet(),
+        new Happiness(),
+        new Personality(),
+        new Likes(),
+        new TruthTable(),
+        false
+    );
+}
+
+describe("Agent should", () => {
+    it("throw error if creating with wrong parameters.", () => {
+        expect(() => new Agent(null, Aspect.generateOriginHuman(OriginKind.Oceanic), new RelationSet(), new Happiness(), new Personality(), new Likes(), new TruthTable(), false)).toThrowError();
+        expect(() => new Agent("First", null, new RelationSet(), new Happiness(), new Personality(), new Likes(), new TruthTable(), false)).toThrowError();
+        expect(() => new Agent("First", Aspect.generateOriginHuman(OriginKind.Oceanic), null, new Happiness(), new Personality(), new Likes(), new TruthTable(), false)).toThrowError();
+        expect(() => new Agent("First", Aspect.generateOriginHuman(OriginKind.Oceanic), new RelationSet(), null, new Personality(), new Likes(), new TruthTable(), false)).toThrowError();
+        expect(() => new Agent("First", Aspect.generateOriginHuman(OriginKind.Oceanic), new RelationSet(), new Happiness(), null, new Likes(), new TruthTable(), false)).toThrowError();
+        expect(() => new Agent("First", Aspect.generateOriginHuman(OriginKind.Oceanic), new RelationSet(), new Happiness(), new Personality(), null, new TruthTable(), false)).toThrowError();
+        expect(() => new Agent("First", Aspect.generateOriginHuman(OriginKind.Oceanic), new RelationSet(), new Happiness(), new Personality(), new Likes(), null, false)).toThrowError();
+    });
+
+    it("say something", () => {
+        let agent = generateAgent("First agent");
+        let something = agent.say("Hello");
+        expect(something).toBe("First agent: - Hello");
+    });
+
+    it("return an equivalent individual", () => {
+        let agent = generateAgent("First agent");
+        let individual = agent.Individual;
+        expect(individual.equals(new Individual("Firstagent"))).toBe(true);
+    });
+});
+
+describe("Crowd should", () => {
+    it("throw error if wrong constructor", () => {
+        expect(() => new Crowd(null)).toThrowError();
+    });
+
+    it("create a crowd of agents", () => {
+        let agent1 = generateAgent("First");
+        let agent2 = generateAgent("Second");
+        let crowd = new Crowd([ agent1, agent2 ]);
+
+        expect(crowd.all.length).toBe(2);
+        expect(crowd.all[0].Name).toBe("First");
+        expect(crowd.all[1].Name).toBe("Second");
+        expect(crowd.get("First").Name).toBe("First");
+        expect(crowd.get("Second").Name).toBe("Second");
+    });
+
+    it("create a copy", () => {
+        let agent1 = generateAgent("First");
+        let agent2 = generateAgent("Second");
+        let crowd = new Crowd([ agent1, agent2 ]);
+        let copy = crowd.copy();
+
+        expect(copy.all.length).toBe(2);
+        expect(copy.all[0].Name).toBe("First");
+        expect(copy.all[1].Name).toBe("Second");
+    });
+});
+
+describe("Desire should", () => {
+    it("throw error when wrong input.", () => {
+        expect(() => new Desire(null, ["one", "two"])).toThrowError();
+        expect(() => new Desire((crowd:Crowd) => 0, null)).toThrowError();
+    });
+
+    it("create a desire implementation.", () =>{
+        let agent1 = generateAgent("First");
+        let agent2 = generateAgent("Second");
+        let desire = new Desire((crowd: Crowd) => 7, [ "First" ]);
+
+        expect(desire.heuristic(new Crowd([ agent1 ]))).toBe(7);
+        expect(desire.anyInvolved([ agent1 ])).toBe(true);
+        expect(desire.anyInvolved([ agent2 ])).toBe(false);
+    });
+});
+
+describe("Desires should", () => {
+    it("create a list of desires", () => {
+        let agent1 = generateAgent("First");
+        let agent2 = generateAgent("Second");
+
+        let desire1 = new Desire((crowd: Crowd) => 7, [ "First" ]);
+        let desire2 = new Desire((crowd: Crowd) => 10, [ "First" ]);
+
+        let desires = new Desires()
+            .append(desire1)
+            .append(desire2);
+
+        expect(desires.any).toBe(true);
+        expect(desires.anyInvolved([ agent1 ])).toBe(true);
+        expect(desires.anyInvolved([ agent2 ])).toBe(false);
+        expect(desires.heuristicTotal(new Crowd([ agent1 ]))).toBe(17);
+
+        desires.clear();
+
+        expect(desires.any).toBe(false);
+    });
+});
