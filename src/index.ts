618e044731c9f82c867f273d7a9c29a426a8d479
Ivan Saez Zamora <ivsaez@gmail.com>
2022-05-15 23:37:46 +0200

    export characteristics

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ export { Input } from "./input";
 export { PhraseResult, Alternative, Phrase } from "./phrase";
 export { RolesDescriptor, Roles } from "./roles";
 export { Choices, Step } from "./step";
-export { Agent, Agents, Desire, Crowd, Desires, PassTurn, Heuristic } from "./agent";
+export { Agent, Agents, Desire, Crowd, Desires, PassTurn, Heuristic, Characteristics } from "./agent";
 export { Location, MapStructure } from "./location";
 export { IDeliverer, IInteraction, Timing } from "./interfaces";
 export { Deliverer, BaseInteraction, Interaction } from "./interaction";
